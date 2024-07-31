62dd50672cb89656aa5a7297c7c01d658660e3a8
Bharati Garg <bharatigargkls1999@gmail.com>
2024-07-31 20:23:28 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import SlotM from "./SlotMach";
+const App = () => {
+  return (
+    <>
+      <h1 className="heading_style">
+        🎰 Welcome to
+        <span style={{ fontWeight: "bold" }}>Slot machine game</span>🎰
+      </h1>
+      <div className="slotmachine">
+        <SlotM x="😄" y="😄" z="😄" />
+        <hr />
+        <SlotM x="😄" y="😆" z="😄" />
+        <hr />
+        <SlotM x="🍎" y="🍌" z="🍎" />
+        <hr />
+        <SlotM x="🍎" y="🍎" z="🍎" />
+      </div>
+    </>
+  );
+};
+
+export default App;
